5f8e7fcba732ee46b4d1b268a08e5d1a8b27c8b0
“mehmetkah” <mehmetkahca@gmail.com>
2023-02-11 15:19:26 -0700

    initial commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,12 @@
-import NavBar from "./NavBar";
 import "./Profile.scss";
 export default function Profile(props) {
   return (
     <div className="profile">
       <div className="userInfo">
-        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FAang&psig=AOvVaw2jD6V7mGKj-150SxwfuDA1&ust=1676237705273000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPCQ1Zm2jv0CFQAAAAAdAAAAABAE"></img>
+        <img
+          alt="logo"
+          src="/Users/mehmetkahraman/final_project/public/Avatar_Aang.png"
+        ></img>
         <h3>Mover</h3>
       </div>
       <div className="userActions">
