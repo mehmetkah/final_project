d1ec3a220db001096a4305182c57ac66cd20294c
Anaaag <agarc20@outlook.com>
2023-02-14 10:02:47 -0800

    styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,10 @@ function NavBar() {
   return (
     <div className="navBar">
       <p className="logo">MovementSnax</p>
-      <a href="/">Home</a>
-      <a href="/Profile">Profile</a>
+     
+        <a href="/">Home</a>
+        <a href="/Profile">Profile</a>
+      
     </div>
   );
 }
